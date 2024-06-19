8d68605de00376e79aecc91504ffdef3df0c6be7
Giorgiana353 <146415215+Giorgiana353@users.noreply.github.com>
2024-06-19 12:41:31 +0300

    FluturasSalariu.java

---
+++ FluturasSalariu.java
@@ -0,0 +1,16 @@
+
+public class FluturasSalariu extends Act {
+	
+	private double valoareSalariu;
+	private Angajat angajat;
+	
+	public double getValoareSalariu() {
+		return valoareSalariu;
+	}
+	public Angajat getAngajat() {
+		return angajat;
+	}
+	
+	
+
+}
