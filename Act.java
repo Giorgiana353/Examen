72f7d694ea1d0b9724707ccf26050b9e2c5a4137
Giorgiana353 <146415215+Giorgiana353@users.noreply.github.com>
2024-06-19 12:39:36 +0300

    Act.java

---
+++ Act.java
@@ -0,0 +1,16 @@
+
+public abstract class Act {
+	
+	private String id;
+	private String numarIdentificare;
+	
+	public String getId() {
+		return id;
+	}
+	public String getNumarIdentificare() {
+		return numarIdentificare;
+	}
+	
+	
+
+}
