f9a27a98077b9b31a0980855e4fb06f7fdf3501a
Giorgiana353 <146415215+Giorgiana353@users.noreply.github.com>
2024-06-19 12:43:02 +0300

    Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Angajat muncitor = new Muncitor("Dinu","Giorgiana","muncitor",2000, 1, null);
+		Angajat manager = new Manager("Ion","Maria","manager", 4000, 5, null);
+		
+		System.out.println();
+	}
+
+}
